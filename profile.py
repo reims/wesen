088ccddd638cd8d11b5172adcdded4e460f03a0f
konradvoelkel <web@konradvoelkel.de>
2013-06-23 22:02:02 +0200

    disabled logging during profiling; made stats more useful by limiting total stats lines and using total time instead cumulative time

--- profile.py
+++ profile.py
@@ -9,6 +9,7 @@ print("You can supply an alternative config file on the command-line");
 print("You can stop profiling by Ctrl+C");
 
 sys.argv.append('--disablegui');
+sys.argv.append('--disablelog');
 cProfile.run("Loader()", "profile.stats");
 
 stats = pstats.Stats('profile.stats')
@@ -16,7 +17,8 @@ stats = pstats.Stats('profile.stats')
 # Clean up filenames for the report
 stats.strip_dirs()
 
-# Sort the statistics by the cumulative time spent in the function
-stats.sort_stats('cumulative')
+# Sort the statistics by the total time spent in the function
+stats.sort_stats('tottime')
 
-stats.print_stats()
+# Print the 35 most time consuming methods
+stats.print_stats(35)
