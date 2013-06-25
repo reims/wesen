7e47f1b21850e6400f8743de3eb73f657f8d644a
konradvoelkel <web@konradvoelkel.de>
2013-06-25 22:20:21 +0200

    moved some small functions to the single place which called them, for performance.

--- src/Wesen/point.py
+++ src/Wesen/point.py
@@ -3,10 +3,8 @@ This program is distributed under the terms of the GNU General Public License.
 visit http://www.sourceforge.net/projects/wesen or
 http://wesen.sourceforge.net for newer versions."""
 
-from random import randint;
+from numpy.random import randint;
 from math import copysign;
-
-ZERO_POSITION = [0,0];
 
 def getRandomPosition(length):
 	"""returns a random n-dimensional position."""
@@ -17,17 +15,7 @@ def getRandomPositionInRadius(position, radius, length):
 	return [(length + pc + randint(-radius,radius)) % length for pc in position];
 
 def signum(number):
-	if(number < 0): return -1;
-	elif(number > 0): return 1;
-	else: return 0;
-
-def getNewPosition(position, direction, length):
-	"""returns the position resulting from going into "direction" from "position"."""
-	return [(pc+dc) % length for (pc,dc) in zip(position,direction)];
-
-def positionToDirection(position, newposition):
-	"""returns the direction from one point to another one."""
-	return [signum(nc-pc) for (nc,pc) in zip(newposition, position)];
+	return -1 if number < 0 else 1 if number > 0 else 0;
 
 def getShortestTranslation(a, b, length):
 	return [min(c,-1*copysign(length-c,c),key=abs) for c in [(bc-ac) % length for (ac,bc) in zip(a,b)]];
