7157f0f93fd67e5a2b4cfce702c1425f068c3926
konradvoelkel <web@konradvoelkel.de>
2013-06-25 22:12:50 +0200

    more interesting stats now

--- profile.py
+++ profile.py
@@ -1,8 +1,9 @@
 #! /usr/bin/python3
 
 from src.Wesen.loader import Loader;
-import cProfile;
-import pstats;
+from cProfile import Profile;
+from time import perf_counter;
+from pstats import Stats;
 import sys;
 
 print("You can supply an alternative config file on the command-line");
@@ -10,15 +11,10 @@ print("You can stop profiling by Ctrl+C");
 
 sys.argv.append('--disablegui');
 sys.argv.append('--disablelog');
-cProfile.run("Loader()", "profile.stats");
+pr = Profile(perf_counter);
+pr.run('Loader()');
 
-stats = pstats.Stats('profile.stats')
+stats = Stats(pr)
 
-# Clean up filenames for the report
-stats.strip_dirs()
-
-# Sort the statistics by the total time spent in the function
 stats.sort_stats('tottime')
-
-# Print the 35 most time consuming methods
-stats.print_stats(35)
+stats.print_stats(20)
