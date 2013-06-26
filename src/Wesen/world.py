e75d4f9276d467664afe8ecd6c5f230b4725f0e4
konradvoelkel <web@konradvoelkel.de>
2013-06-26 19:08:15 +0200

    Two internal changes of data structure:
    * the big list of all objects is now stored in an id-indexed dict.
      in future, this should be kept internally as a map backed by a sorted list,
      to exploit the fact that we don't need to hash and can use the unique id for this.
    * we don't use any object.copy() for our security model any longer,
      instead we use python's internal Proxy mechanism.
      This doesn't change any behaviour,
      but it will throw exceptions if someone tries to break the rules.

--- src/Wesen/world.py
+++ src/Wesen/world.py
@@ -5,6 +5,15 @@ http://wesen.sourceforge.net for newer versions."""
 
 from .objects.wesen import Wesen;
 from .objects.food import Food;
+from ctypes import pythonapi, py_object;
+from _ctypes import PyObj_FromPtr;
+DictProxy = pythonapi.PyDictProxy_New;
+DictProxy.argtypes = (py_object,);
+DictProxy.rettype = py_object;
+
+def make_dictproxy(obj):
+	assert isinstance(obj,dict);
+	return PyObj_FromPtr(DictProxy(obj));
 
 class World(object):
 	"""World(infoObject) creates a World instance.
@@ -21,7 +30,7 @@ class World(object):
 		self.logger = self.infoWorld["logger"];
 		self.finished = False;
 		self.winner = None;
-		self.objects = [];
+		self.objects = dict();
 		self.turns = 0;
 		self.infoWorld.update(dict(DeleteObject=self.DeleteObject, AddObject=self.AddObject, objects=self.objects));
 		self.infoFood["type"] = "food";
@@ -29,17 +38,22 @@ class World(object):
 		for entry in self.infoWesen["sources"]:
 			for i in range(self.infoWesen["count"]):
 				self.infoWesen["source"] = entry;
-				self.AddObject(self.infoWesen.copy());
+				self.AddObject(make_dictproxy(self.infoWesen));
 		for i in range(self.infoFood["count"]):
 			self.AddObject(self.infoFood);
 
 	def DeleteObject(self, objectid):
 		"""removes an object from the world."""
-		for o in self.objects:
-			if(o.id == objectid):
-				del(self.objects[self.objects.index(o)]);
-				return True;
-		return False;
+		if(objectid in self.objects.keys()):
+			del self.objects[objectid];
+			return True;
+		else:
+			return False;
+		#for o in self.objects.values():
+		#	if(o.id == objectid):
+		#		del(self.objects[self.objects.index(o)]);
+		#		return True;
+		#return False;
 
 	def AddObject(self, infoObject):
 		"""adds an object to the world."""
@@ -51,14 +65,14 @@ class World(object):
 		else:
 			self.Debug("critical\tWorld.AddObject\tinvalid objectType specified in infoObject", level="error");
 			newObject = None;
-		self.objects.append(newObject);
+		self.objects[newObject.id] = newObject;
 		return newObject;
 
 	def getDescriptor(self):
 		"""returns a list of description information for the GUI,
 		[{"finished":Boolean}[...]]
 		where [...] is a list of all objects descriptors in the world."""
-		return [dict(finished=self.finished), [object.getDescriptor() for object in self.objects]];
+		return [dict(finished=self.finished), [object.getDescriptor() for object in self.objects.values()]];
 
 	def getEnergy(self):
 		return self.energy;
@@ -71,16 +85,16 @@ class World(object):
 		for source in self.infoWesen["sources"]:
 			sources[source]=dict(count=0,energy=0);
 		globalEnergy = 0;
-		for object in self.objects:
-			globalEnergy += object.energy;
-			if(type(object) is Wesen):
-				sources[object.source]["count"] += 1;
-				sources[object.source]["energy"] += object.energy;
+		for o in self.objects.copy().values():
+			globalEnergy += o.energy;
+			if(o.objectType == "wesen"):
+				sources[o.source]["count"] += 1;
+				sources[o.source]["energy"] += o.energy;
 				stillActive = True;
 			else:
 				sources["food"]["count"] += 1;
-				sources["food"]["energy"] += object.energy;
-			object.main();
+				sources["food"]["energy"] += o.energy;
+			o.main();
 		self.energy = globalEnergy;
 		self.stats = sources;
 		#if(len(list(sources.keys())) == 2):
