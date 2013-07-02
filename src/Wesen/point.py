4ec373508c291c98a5701f348fe20c6cf6e337b9
konradvoelkel <web@konradvoelkel.de>
2013-07-02 10:42:49 +0200

    code beautification

--- src/Wesen/point.py
+++ src/Wesen/point.py
@@ -7,22 +7,27 @@ from math import copysign;
 
 def getRandomPosition(length):
 	"""returns a random n-dimensional position."""
-	return [randint(0,length-1), randint(0,length-1)];
+	return [randint(0, length-1), randint(0, length-1)];
 
 def getRandomPositionInRadius(position, radius, length):
 	"""x + random(-radius,+radius)"""
-	return [(length + pc + randint(-radius,radius)) % length
+	return [(length + pc + randint(-radius, radius)) % length
 		for pc in position];
 
 def signum(number):
+	"""return the sign +-1 or 0 if 0"""
 	return -1 if number < 0 else 1 if number > 0 else 0;
 
 def getShortestTranslation(a, b, length):
-	return [min(c,-1*copysign(length-c,c),key=abs)
-		for c in [(bc-ac) % length for (ac,bc) in zip(a,b)]];
+	"""takes ((ax,ay),(bx,by),length),
+	computes shortest vector from a to b."""
+	return [min(c, -1*copysign(length-c, c), key=abs)
+		for c in [(bc-ac) % length for (ac, bc) in zip(a, b)]];
 
 def getDistInMaxMetric(a, b, length):
-	return abs(max(getShortestTranslation(a,b,length), key=abs));
+	"""takes ((ax,ay),(bx,by),length),
+	computes distance from a to b."""
+	return abs(max(getShortestTranslation(a, b, length), key=abs));
 
 
 
