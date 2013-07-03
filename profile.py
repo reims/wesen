15a43524f2bbb7dd37bb2bdee8bf1d9801ce5958
konradvoelkel <web@konradvoelkel.de>
2013-07-03 22:27:48 +0200

    More documentation for using the script to profile AI code

--- profile.py
+++ profile.py
@@ -1,20 +1,32 @@
 #! /usr/bin/python3
 
+"""This profiling routine was used to identify bottlenecks,
+then in objects.base the getRange method was optimized.
+It still is the main bottleneck,
+but there seems to be no potential for further optimization.
+
+Now you should modify/copy this file,
+to profile your AI source code.
+
+In some tournaments, there is a penalty for computation cost."""
+
 from src.Wesen.loader import Loader;
 from cProfile import Profile;
-from time import perf_counter;
+from time import perf_counter; # only in python3.3
 from pstats import Stats;
 import sys;
 
 print("You can supply an alternative config file on the command-line");
-print("You can stop profiling by Ctrl+C");
+print("You should stop Wesen by Ctrl+C to finish profiling");
 
 sys.argv.append('--disablegui');
 sys.argv.append('--disablelog');
 pr = Profile(perf_counter);
 pr.run('Loader()');
+pr.dump_stats('profile.stats');
+# you may explore profile.stats with the pstats browser.
 
 stats = Stats(pr)
 
 stats.sort_stats('tottime')
-stats.print_stats(20)
+stats.print_stats('Wesen/sources',10)
